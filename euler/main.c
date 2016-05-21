9178840d89292816695728c8f969e953f512d392
Yauheni Khvaliuk <hvaluk@gmail.com>
2016-05-21 12:08:46 +0000

    print list (bad way)

--- euler/main.c
+++ euler/main.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+/* linked list type */
+typedef struct node {
+    int val;
+    struct node * next;
+} node_t;
+
+void push(node_t *head, int val);
+void print_list(node_t *head);
 
 int main(int argc, char** argv)
 {
@@ -17,27 +27,77 @@ int main(int argc, char** argv)
 	}
 
 	/* read from file */
-	int a, b, n;
+	int a, b, v;
 	int x;
 
-	x = fscanf(fi, "%d", &n);
+	x = fscanf(fi, "%d", &v);
 
 	if (x == EOF || x == 0) {
 		fprintf(stderr, "invalid input\n");
 		return 1;
 	}
 
-	printf("%d\n", n);
+	node_t *adj[v];
+	int i;
+	for (i = 0; i < v; i++)
+	{
+		adj[i] = NULL;
+	}
 
 	while ((x = fscanf(fi, "%d %d", &a, &b)) != EOF) {
 		if (x == 0) {
 			fprintf(stderr, "invalid input\n");
 			return 1;
 		}
-		printf("%d, %d\n", a, b);
-	}
 
+		if (adj[a] == NULL) {
+			adj[a] = malloc(sizeof(node_t));
+			adj[a]->val = b;
+			adj[a]->next = NULL;
+		}
+		else {
+			push(adj[a], b);
+		}
+
+		if (adj[b] == NULL) {
+			adj[b] = malloc(sizeof(node_t));
+			adj[b]->val = a;
+			adj[b]->next = NULL;
+		}
+		else {
+			push(adj[b], a);
+		}
+
+	}
 
 	/* close file */
 	fclose(fi);
+
+	for (i = 0; i < v; i++)
+	{
+		fprintf(stderr, "----%d\n", i);
+		print_list(adj[i]);
+	}
+}
+
+
+void print_list(node_t * head) {
+    node_t * current = head;
+
+    while (current != NULL) {
+        fprintf(stderr, "%d\n", current->val);
+        current = current->next;
+    }
+}
+
+void push(node_t *head, int val) {
+	node_t * current = head;
+
+    while (current->next != NULL) {
+        current = current->next;
+    }
+
+    current->next = malloc(sizeof(node_t));
+    current->next->val = val;
+    current->next->next = NULL;
 }
